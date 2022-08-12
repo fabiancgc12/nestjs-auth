a0f4ebad1726b0b32bfb7491935292e360bc5f44
fabian graterol <fabiancgraterol@gmail.com>
2022-08-12 00:49:24 -0400

    agregando cors al sistema

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.use(cookieParser());
+  app.enableCors({
+    origin:"http://localhost:4000",
+    credentials:true
+  })
   await app.listen(3000);
 }
 bootstrap();
